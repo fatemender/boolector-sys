affd901f2f8dd4de497c73e14a1cd23b51726102
Craig Disselkoen <craigdissel@gmail.com>
2019-09-04 11:05:10 -0700

    Use libc::FILE in generated bindings

--- src-generated/update-bindings.sh
+++ src-generated/update-bindings.sh
@@ -4,5 +4,8 @@ bindgen \
     --output bindings.rs \
     --whitelist-function '^boolector_(.*)$' \
     --whitelist-type '^Btor(.*)$' \
+    --whitelist-type '^Boolector(.*)$' \
+    --no-recursive-whitelist \
+    --raw-line 'use libc::FILE;' \
     --no-doc-comments \
     ../boolector/src/boolector.h
