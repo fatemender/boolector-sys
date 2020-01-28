1b35b41d4f1fe1ba0b75dd80c79dfe3c82054a70
Craig Disselkoen <craigdissel@gmail.com>
2020-01-28 09:55:56 -0800

    Also generate bindings for some enums used in options

--- src-generated/update-bindings.sh
+++ src-generated/update-bindings.sh
@@ -5,7 +5,20 @@ bindgen \
     --whitelist-function '^boolector_(.*)$' \
     --whitelist-type '^Btor(.*)$' \
     --whitelist-type '^Boolector(.*)$' \
+    --blacklist-type '^BtorOpt(.*)$' \
     --no-recursive-whitelist \
     --raw-line 'use libc::FILE;' \
     --no-doc-comments \
     ../boolector/src/boolector.h
+
+bindgen \
+    --output options.rs \
+    --generate types \
+    --whitelist-type '^BtorOpt(.*)$' \
+    --blacklist-type '^BtorOpt$' \
+    --blacklist-type '^BtorOptHelp$' \
+    --no-recursive-whitelist \
+    --no-doc-comments \
+    ../boolector/src/btoropt.h \
+    -- \
+    -I../boolector/src
