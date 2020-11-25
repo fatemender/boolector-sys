26c01315d7cc980fa75d62124efc9813a7aebdd2
fatemender <icee@icee.ru>
2020-11-25 06:47:58 -0500

    Add support for vendored Boolector builds
    This uses `boolector-src` from `finga` (renamed to `boolector-build`) and currently only supports Lingeling as the SAT backend.

---
+++ boolector-build/README.md
@@ -0,0 +1,5 @@
+# boolector-src
+
+This crate contains the logic to build Boolector and is intended to be
+consumed by the `boolector-sys` crate. You likely in theory aren't
+interacting with this too much!
--- build.rs
+++ build.rs
@@ -1,3 +1,17 @@
 fn main() {
+    #[cfg(feature = "vendor-lgl")]
+    {
+        if std::env::var("BOOLECTOR_NO_VENDOR").map_or(true, |s| s == "0") {
+            let boolector = boolector_build::Build::new()
+                .prerequisites()
+                .build();
+
+            println!("cargo:vendored=1");
+            println!("cargo:root={}", boolector.lib_dir().parent().unwrap().display());
+
+            boolector.print_cargo_metadata();
+        }
+    }
+
     println!("cargo:rustc-link-lib=boolector");
 }
